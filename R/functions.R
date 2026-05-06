4825bdeb71db910e745a16cd1ee3036358a98300
Frederik Østergaard Klit <264459558+FrederikOestergaardKlit@users.noreply.github.com>
2026-05-06 10:52:41 +0200

    Made the map function more general and robust and moved it to R/functions.R

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,7 @@
+# Global variables ----
+.DATASET_DIR <- here::here("data-raw/nurses-stress/")
+
+
 #' Read in one nurses' stress data file.
 #'
 #' @param file_path Path to the data file.
@@ -10,4 +14,24 @@ read <- function(file_path, n_rows = 100) {
     data.table::fread(nrows = n_rows)
   data.table::setnames(dt, snakecase::to_snake_case)
   return(dt)
+}
+
+
+#' Function reading all files within the nurses-stress stress subfolder
+#'
+#' @param filename Specifies the file type you want to load
+#'
+#' @returns a data.table
+#' @export
+#'
+#' @examples
+read_all <- function(filename, n_rows = 100) {
+  files <- .DATASET_DIR |>
+    fs::dir_ls(regexp = filename, recurse = TRUE)
+
+  data <- files |>
+    lapply(\(file) read(file, n_rows = n_rows)) |>
+    data.table::rbindlist(idcol = "source")
+
+  return(data)
 }
